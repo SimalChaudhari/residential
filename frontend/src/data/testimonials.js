bb33c3387cf8cdd68c4b3d83b89dce46db332335
SimalChaudhari <simalchaudhari20496@gmail.com>
2024-12-07 14:06:04 +0530

    First Commit

---
+++ frontend/src/data/testimonials.js
@@ -0,0 +1,42 @@
+module.exports = [
+  {
+    id: 1,
+    title: "Great Work",
+    quote:
+      "Amazing design, easy to customize and a design quality superlative account on its cloud platform for the optimized performance. And we didn’t on our original designs.",
+    stars: 5,
+    image: "/images/testimonials/testimonial-t1.png",
+    name: "Leslie Alexander",
+    company: "Nintendo",
+  },
+  {
+    id: 2,
+    title: "Great Work",
+    quote:
+      "Amazing design, easy to customize and a design quality superlative account on its cloud platform for the optimized performance. And we didn’t on our original designs.",
+    stars: 5,
+    image: "/images/testimonials/testimonial-t2.png",
+    name: "Floyd Miles",
+    company: "Bank of America",
+  },
+  {
+    id: 3,
+    title: "Great Work",
+    quote:
+      "Amazing design, easy to customize and a design quality superlative account on its cloud platform for the optimized performance. And we didn’t on our original designs.",
+    stars: 5,
+    image: "/images/testimonials/testimonial-t3.png",
+    name: "Leslie Alexander",
+    company: "Nintendo",
+  },
+  {
+    id: 4,
+    title: "Great Work",
+    quote:
+      "Amazing design, easy to customize and a design quality superlative account on its cloud platform for the optimized performance. And we didn’t on our original designs.",
+    stars: 5,
+    image: "/images/testimonials/testimonial-t4.png",
+    name: "Floyd Miles",
+    company: "Bank of America",
+  },
+];
